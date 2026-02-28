6915048f9fab6c8161226e72789ee87ace230745
BYUKUSENGE-Ezira <byukusengeezira1@gmail.com>
2026-02-28 21:15:15 +0200

    hello wolrd

---
+++ StudentManagementSystem/src/main/java/com/mycompany/studentmanagementsystem/StudentManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.studentmanagementsystem;
+
+/**
+ *
+ * @author BYUKUSENGE Ezira
+ */
+public class StudentManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
