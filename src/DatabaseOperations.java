64cc968f0caf1020df908f1d00f52d3082d5722d
BYUKUSENGE-Ezira <byukusengeezira1@gmail.com>
2026-03-06 23:33:20 +0400

    Initial commit

---
+++ src/DatabaseOperations.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public interface DatabaseOperations {
+    void add(); // [cite: 39]
+    void delete(); // [cite: 40]
+    void update(); // [cite: 41]
+    void search(String keyword); // [cite: 42]
+}
